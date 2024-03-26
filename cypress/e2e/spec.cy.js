aaa71521abb9251b6f8386d0bdd7904779e841e3
BrianMohamadSafiudin <brianms2004@gmail.com>
2024-03-26 19:41:40 +0700

    Step 4: Make an assertion

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -26,13 +26,47 @@
 
 // Step 3: Click an element
 
+// describe('My First Test', () => {
+//   it('clicks the link "type"', () => {
+//     cy.visit('https://example.cypress.io')
+
+//     cy.contains('type').click()
+//   })
+// })
+
+// ---------------------------------------------
+
+// Step 4: Make an assertion
+
 describe('My First Test', () => {
-  it('clicks the link "type"', () => {
+  it('Gets, types and asserts', () => {
     cy.visit('https://example.cypress.io')
 
     cy.contains('type').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it
+    cy.get('.action-email').type('fake@email.com')
+
+    //  Verify that the value has been updated
+    cy.get('.action-email').should('have.value', 'fake@email.com')
   })
 })
+
+// describe('My First Test', () => {
+//   it('clicking "type" navigates to a new url', () => {
+//     cy.visit('https://example.cypress.io')
+
+//     cy.contains('type').click()
+
+//     // Should be on a new URL which
+//     // includes '/commands/actions'
+//     cy.url().should('include', '/commands/actions')
+//   })
+// })
 
 // ---------------------------------------------
 
