7dab077f444e680716fd076c56346bb088b01635
BrianMohamadSafiudin <brianms2004@gmail.com>
2024-03-26 19:12:32 +0700

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,17 @@
+// Write your first test (True)
+
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
+
+// ---------------------------------------------
+
+// Write your first test (False)
+
+// describe('My First Test', () => {
+//   it('Does not do much!', () => {
+//     expect(true).to.equal(false)
+//   })
+// })
