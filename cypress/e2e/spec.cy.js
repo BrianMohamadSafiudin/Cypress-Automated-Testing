71d8f6d9deb2e43e0f4a19022f122d917c4babd0
BrianMohamadSafiudin <brianms2004@gmail.com>
2024-03-26 19:20:57 +0700

    Step 2: Query for an element

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,24 @@
 // Step 1: Visit a page
 
+// describe('My First Test', () => {
+//   it('Visits the Kitchen Sink', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+
+// ---------------------------------------------
+
+// Step 2: Query for an element
+
 describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
+  it('finds the content "type"', () => {
     cy.visit('https://example.cypress.io')
+
+    // tes untuk sukses
+    cy.contains('type')
+
+    // tes untuk gagal
+    // cy.contains('hype')
   })
 })
 
